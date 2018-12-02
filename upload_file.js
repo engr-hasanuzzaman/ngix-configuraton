45ca2f95deb97d1822b44ee532e3d4a299c0055e
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-02 15:09:55 +0600

    refactor
    -add code to upload zip to server, unzipa and move frontend folder

--- upload_file.js
+++ upload_file.js
@@ -1,11 +1,16 @@
 const path = require('path');
 const exec = require('child_process').exec;
 const fs = require('fs');
+
+// ssh related info
 const pem_path = '/home/sumon/moneyking.pem';
+const ssh_user = 'ubuntu@ec2-13-251-130-124.ap-southeast-1.compute.amazonaws.com';
+const ssh_access_cmd = 'ssh -i ' + pem_path + ' ' + ssh_user;
+// build related info
 const build_path = path.join(__dirname, 'build');
 const build_zip_name = 'build.zip';
 const build_zip_path = path.join(__dirname, build_zip_name);
-const dest_path = '/';
+const dest_path = '/var/www/html/frontend';
 // guird clause
 if(!fs.existsSync(build_path)){
   error_msg('--- passing build path ' + build_path + ' is not exist');
@@ -27,7 +32,37 @@ exec('zip -r build.zip ' + build_path, (err, stdout, stderr) => {
 function upload_file(pem_path, target_path, des_path, call_back){
   if(fs.existsSync(pem_path) && fs.existsSync(target_path)){
     info_msg('----pem and targe path exist, now upload');
-    call_back();
+    let host_path = ssh_user + ':' + des_path;
+    let ssh_cmd = ssh_access_cmd + ' cd /var/www/html/frontend | unzip -o build.zip';
+    let ssh_mv = ssh_access_cmd + ' mv /var/www/html/frontend/build/* /var/www/html/frontend';
+    
+    // scp build.zip to server
+    info_msg('uploading build.zip to server');
+    exec('scp -i ' + pem_path + ' ' + target_path + ' ' + host_path, (err, stdout, stderr) => {
+      if(err){
+        error_msg('--upload file with scp fail ' + err );
+        process.exit(1)
+      }
+
+      info_msg('unzip build.zip to frontend');
+      exec(ssh_cmd, (err, stdout, stderr) => {
+        if(err){
+          error_msg('unzipping build.zip to server fail', err);
+          process.exit(1);
+        }
+        succ_msg('unzip success');  
+        info_msg('move all files to frontend folder in server');
+        exec(ssh_mv, (err, stdout, stderr) => {
+          if(err){
+            error_msg('could not move files to frontend folder ', err);
+          }
+          succ_msg('move all the files from frontend/build to frontend');
+        })
+      })
+      info_msg('-- ssh_cmd is' + ssh_cmd);
+      info_msg('-- ssh_mv is' + ssh_mv);
+      call_back();
+    })
     // exec('scp -i ' + pem_path + ' ')
   }else{
     error_msg('pem file or target is missing');
@@ -50,31 +85,3 @@ function succ_msg(msg){
 function info_msg(msg){
   colorized(msg, '\x1b[33m');
 }
-
-/*
-Reset = "\x1b[0m"
-Bright = "\x1b[1m"
-Dim = "\x1b[2m"
-Underscore = "\x1b[4m"
-Blink = "\x1b[5m"
-Reverse = "\x1b[7m"
-Hidden = "\x1b[8m"
-
-FgBlack = "\x1b[30m"
-FgRed = "\x1b[31m"
-FgGreen = "\x1b[32m"
-FgYellow = "\x1b[33m"
-FgBlue = "\x1b[34m"
-FgMagenta = "\x1b[35m"
-FgCyan = "\x1b[36m"
-FgWhite = "\x1b[37m"
-
-BgBlack = "\x1b[40m"
-BgRed = "\x1b[41m"
-BgGreen = "\x1b[42m"
-BgYellow = "\x1b[43m"
-BgBlue = "\x1b[44m"
-BgMagenta = "\x1b[45m"
-BgCyan = "\x1b[46m"
-BgWhite = "\x1b[47m
-*/
